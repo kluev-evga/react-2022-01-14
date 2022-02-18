7ca0a1d538224a703f76033ffa8494cd417723ad
Евгений Клюев <craftycoderpro@gmail.com>
2022-02-18 14:01:55 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import { ConnectedRouter } from 'connected-react-router';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './components/app';
 import './index.css';
@@ -8,16 +9,14 @@ import { CurrencyProvider } from './contexts/currency-context';
 import history from './history';
 import store from './redux/store';
 
-// DEV ONLY!!!
-window.store = store;
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
+    <BrowserRouter>
       <CurrencyProvider>
         <App />
       </CurrencyProvider>
-    </ConnectedRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
